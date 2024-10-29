a94446351604a2d670e2ba355b2c959ed52e70a1
debartha-clearspot <debartha2riki@gmail.com>
2024-10-30 01:05:12 +0530

    Initial Commit

---
+++ movies/src/test/java/dev/debartha/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.debartha.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
